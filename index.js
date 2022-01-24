b24bd21a9b9fc540773c1eb5b5c9212eabe41227
yoricktf <yorick.tenfeld@gmail.com>
2022-01-24 15:38:44 +0100

    random number logic

--- index.js
+++ index.js
@@ -16,21 +16,70 @@ recalculates the prices
 
 let daysLeft = 3
 let money = 2000
+let vegetables = {
+  Cabbage: randomValue(2,5),
+  Bananas: randomValue(5,10),
+  Oranges: randomValue(10,10),
+}
+
 let jacket = {
   cabbage: 0,
   bananas: 0,
-  oranges: 0
+  oranges: 0,
 }
 
-function randomValue (min, multiplyer ) {
+
+
+function randomValue (min, multiplyer) {
   let value = min + Math.floor(Math.random()* multiplyer)
-  console.log(value)
+  return value
 }
 
 function updateSubtotal () {
-  let price = document.querySelector('.price span').innerHTML
-  console.log(price);
-  return price
+  const products = document.getElementsByClassName('product')
+  // let price = document.querySelector('.price span').textContent
+  // let quantity = document.querySelector('.quantity input').value
+  // let subtotal = price * quantity
+  // document.querySelector('.subtotal span').textContent = subtotal
+  for (product of products) {
+    console.log(product)
+    let price = product.querySelector('.price span').textContent
+    let quantity = product.querySelector('.quantity input').value
+    let subtotal = price * quantity
+    product.querySelector('.subtotal span').textContent = subtotal
+  }
 }
-updateSubtotal()
-randomValue(10,2)
+
+// changing subtotal when the quantity is changed
+
+  document.querySelector('.quantity input').onkeyup = function () {updateSubtotal()}
+
+
+
+
+function calculateAll() {
+  const products = document.getElementsByClassName('product')
+  let keys = []
+  let values = []
+  let counter = 0
+
+  for (let [key, value] of Object.entries(vegetables)) {
+    keys.push(key)
+    values.push(value)
+  };
+
+  for (product of products) {
+    product.querySelector('.name span').textContent = keys[counter]
+    product.querySelector('.price span').textContent = values[counter]
+    counter ++
+  }
+}
+
+
+
+
+
+
+calculateAll()
+// updateSubtotal()
+// randomValue(10,2)
